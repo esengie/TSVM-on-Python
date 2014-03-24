fixed underflow in svm_ind and written heuristic parameter search

svm_ind.py
@@ -43,7 +43,7 @@ def svm_ind(X, Y, C, k):
     #%%%%%%%%%%%%%%%%%%%%%%%
     #% Get support vectors %
     #%%%%%%%%%%%%%%%%%%%%%%%
-    sv = (A > 1e-5) == (A < (C-1e-5))
+    sv = (A > 1e-10)# == (A < (C-1e-5))
     ind = np.arange(len(A))[sv]
     SA = A[sv]
     SX = X[sv]

test2.py
@@ -1,11 +1,11 @@
 
 #svm_ovo
 import numpy as np
-import kernel_gen_lin as ff
+import kernel_gen_pol as ff
 import svm_ovo as svm1
 import svm_ova as svm2
 
-nnm = ff.kernel_gen_lin()
+nnm = ff.kernel_gen_pol([2,5])
 n = np.array([])
 x1 = np.zeros((12,1))
 y1 = np.zeros(12)
@@ -37,6 +37,10 @@ y1[8]=-1
 y1[9]=-1
 y1[10]=-1
 y1[11]=-1
+
+x1 = np.array([[3,3],[1,7],[2,9], [1,6]])
+y1 = np.array([1,2,3,2])
+
 
 f, SX, SY, SA, t = svm1.svm_ovo(x1, y1, n, C1, C2, nnm)
 f, SX, SY, SA, t = svm2.svm_ova(x1, y1, n, C1, C2, nnm) 

tests.py
@@ -3,10 +3,10 @@
 import numpy as np
 import svm_trn as tt
 import svm_ind as ind
-import kernel_gen_lin as ff
+import kernel_gen_pol as ff
 import svm_ovo
 
-nnm = ff.kernel_gen_lin()
+nnm = ff.kernel_gen_pol([1,2])
 x1 = np.zeros((4,1))
 x2 = np.zeros((4,1))
 x3 = np.zeros((4,1))
@@ -22,6 +22,7 @@ x3[0]=100
 x3[1]=1
 x3[2]=12
 x3[3]=46
+
 C1 = 10
 C2 = 5
 g, h, i, o, io = tt.svm_trn(x1,x2,[],C1,C2,nnm)